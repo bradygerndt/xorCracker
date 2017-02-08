86e44eb2bdfc6b1b2c6533548f6577c1e217606f
Brady <brady.gerndt@gmail.com>
2017-02-08 00:18:37 -0600

    Adding freq.py

---
+++ freq.py
@@ -0,0 +1,68 @@
+
+#gets a dictionary of unique values and their counts
+def freq(input, skip, startat):
+    binDict = {}
+    f = open(input, 'rb')
+    for x in range(startat):
+        f.read(1)
+    while True:
+        chunk = f.read(1)
+        if not chunk:
+            break
+        for i in range(skip - 1):
+            f.read(1)
+        else:
+            if chunk in binDict.keys():
+                binDict[chunk] += 1
+            else:
+                binDict[chunk] = 1
+    f.close()
+    return binDict
+
+#returns a dictionary with coincidences for each character.
+# as the Counts of displacement between characters
+def coinc(uniqueChars, input):
+    coinciDict = {}
+    distance = 0
+    for i in uniqueChars:
+        freqByte = i
+        f=open(input, 'rb')
+        firstFind = False
+        while True:
+            chunk = f.read(1)
+            if not chunk:
+                break
+            if chunk == freqByte:
+                firstFind = True
+                if distance in coinciDict.keys():
+                    coinciDict[distance] +=1
+                elif distance > 0:
+                    coinciDict[distance] = 1
+                distance = 1
+            elif firstFind == True:
+                distance +=1
+    f.close()
+    return coinciDict
+
+#Prints out a dictionary to CSV for frequency analysis
+def writeDictCSV(dict, fileOutName):
+    fo = open(fileOutName + '.csv', 'w')
+    fo.write('Distance' + ','+'Count' + '\n')
+    for i in dict:
+        fo.write(str(i) + "," + str(dict[i]) + "\n")
+    fo.close()
+
+
+binDictOut = freq('out.txt', 13, 0)
+coinciDict = coinc(binDictOut, 'out.txt')
+writeDictCSV(coinciDict, 'resultsCSV')\
+
+#need to find a way to automate frequency analysis
+
+keylength = 13
+password = ""
+
+for i in range(keylength):
+    binDictOut = freq('out.txt', keylength, i)
+    password += max(binDictOut, key=binDictOut.get)
+print password
